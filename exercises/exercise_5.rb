e935752473115b6a193db2c60135221f004477b8
noahthomlison <noahthomlison@gmail.com>
2022-01-04 20:21:23 -0800

    adds a sum, average and where count output

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,3 +1,5 @@
+# bundle exec ruby exercises/exercise_5.rb
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -8,3 +10,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+puts Store.average(:annual_revenue)
+puts Store.where("annual_revenue < 1000000").count
