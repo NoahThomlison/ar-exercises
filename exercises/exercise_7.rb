88d82e8dcbe73543d5828732bc94bbaf7ae72b2d
noahthomlison <noahthomlison@gmail.com>
2022-01-05 06:40:28 -0800

    adds failing stores and employees to test validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,3 +1,5 @@
+# bundle exec ruby exercises/exercise_7.rb
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -10,3 +12,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+Employee.create(first_name: "10", last_name: "lastName", hourly_rate: 60)
+@store2.employees.create(last_name: "lastName", hourly_rate: 60)
+@store2.employees.create(first_name: "10", hourly_rate: 60)
+@store2.employees.create(first_name: "10", last_name: "lastName")
+
+surrey2 = Store.create(name: "Su", annual_revenue: 224000, mens_apparel: false,womens_apparel: true)
+whistler2  = Store.create(name: "Whistler2", annual_revenue: 0 , mens_apparel: true, womens_apparel: false)
+
+puts "What would you like to name your store?"
+@userStoreName = gets
+@userStore = Store.create(name: @userStoreName)
+puts @userStore.errors.messages
