7fc857280dc290b347f5ad53624eb10e4f477602
noahthomlison <noahthomlison@gmail.com>
2022-01-04 19:31:27 -0800

    adds two instances variables for teh first and second store then updates the first stores annual sales

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,3 +1,5 @@
+# bundle exec ruby exercises/exercise_2.rb
+
 require_relative '../setup'
 require_relative './exercise_1'
 
@@ -5,3 +7,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.annual_revenue = 350000
+@store1.save
